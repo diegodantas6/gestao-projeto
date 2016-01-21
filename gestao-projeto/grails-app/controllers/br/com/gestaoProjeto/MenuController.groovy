660fa29a60228c1eacadc43afa426c5aa4164e57
diego.teixeira <diego.teixeira@avistanet.com.br>
2016-01-21 14:47:16 -0200

    starting dashboard

--- gestao-projeto/grails-app/controllers/br/com/gestaoProjeto/MenuController.groovy
+++ gestao-projeto/grails-app/controllers/br/com/gestaoProjeto/MenuController.groovy
@@ -6,5 +6,38 @@ import grails.plugin.springsecurity.annotation.Secured
 class MenuController {
 
 	def index() {
+
+		List projetos = Projeto.list(sort: "nome")
+		
+		for (projeto in projetos) {
+			
+			List atividades = Atividade.findAllByProjeto(projeto)
+			
+			int quantidade = 0
+			
+			int totalConcluido = 0
+			
+			int totalFaltando = 0
+			
+			for (atividade in atividades) {
+				
+				quantidade++
+				
+				totalConcluido += atividade.percentualConcluido
+				
+			}
+			
+			if (quantidade > 0) {
+				// calcula total concluido
+				totalConcluido = ( totalConcluido / quantidade )
+	
+				// calcula total faltando
+				totalFaltando = 100 - totalConcluido
+			}
+
+			println projeto.nome
+			println totalConcluido
+			println totalFaltando
+		}
 	}
 }
