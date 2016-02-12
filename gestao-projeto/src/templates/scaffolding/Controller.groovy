966ba182ce811e499d5d130b331f1cf2d206f49d
diego.teixeira <diego.teixeira@avistanet.com.br>
2016-02-12 14:20:34 -0200

    fix generate controller

--- gestao-projeto/src/templates/scaffolding/Controller.groovy
+++ gestao-projeto/src/templates/scaffolding/Controller.groovy
@@ -1,102 +1,97 @@
 <%=packageName ? "package ${packageName}\n\n" : ''%>
 
-import static org.springframework.http.HttpStatus.*
-import grails.transaction.Transactional
+import grails.converters.JSON
+import grails.plugin.springsecurity.annotation.Secured
+import br.com.teste.enums.NotifyType
+import br.com.teste.utils.UtilsMensagem
 
-@Transactional(readOnly = true)
+@Secured("IS_AUTHENTICATED_FULLY")
 class ${className}Controller {
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+	@Secured(["ROLE_CREATE_${className}", "ROLE_READ_${className}", "ROLE_UPDATE_${className}", "ROLE_DELETE_${className}"])
+	def index() {
+	}
 
-    def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond ${className}.list(params), model:[${propertyName}Count: ${className}.count()]
-    }
+	def listar() {
 
-    def show(${className} ${propertyName}) {
-        respond ${propertyName}
-    }
+		def lista = ${className}.createCriteria().list{ order("nome") }
 
-    def create() {
-        respond new ${className}(params)
-    }
+		render(template: "lista", model:[lista: lista])
+	}
 
-    @Transactional
-    def save(${className} ${propertyName}) {
-        if (${propertyName} == null) {
-            notFound()
-            return
-        }
+	@Secured("ROLE_CREATE_${className}")
+	def incluir() {
 
-        if (${propertyName}.hasErrors()) {
-            respond ${propertyName}.errors, view:'create'
-            return
-        }
+		${className} ${domainClass.propertyName} = new ${className}()
+		
+		render(template: "form", model:[title: "Novo", editable: true, ${domainClass.propertyName}: ${domainClass.propertyName}])
+	}
 
-        ${propertyName}.save flush:true
+	@Secured("ROLE_UPDATE_${className}")
+	def alterar() {
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), ${propertyName}.id])
-                redirect ${propertyName}
-            }
-            '*' { respond ${propertyName}, [status: CREATED] }
-        }
-    }
+		${className} ${domainClass.propertyName} = ${className}.get(params.id)
 
-    def edit(${className} ${propertyName}) {
-        respond ${propertyName}
-    }
+		render(template: "form", model:[title: "Alterar", editable: true, ${domainClass.propertyName}: ${domainClass.propertyName}])
+	}
 
-    @Transactional
-    def update(${className} ${propertyName}) {
-        if (${propertyName} == null) {
-            notFound()
-            return
-        }
+	@Secured("ROLE_READ_${className}")
+	def visualizar() {
 
-        if (${propertyName}.hasErrors()) {
-            respond ${propertyName}.errors, view:'edit'
-            return
-        }
+		${className} ${domainClass.propertyName} = ${className}.get(params.id)
 
-        ${propertyName}.save flush:true
+		render(template: "form", model:[title: "Visualizar", editable: false, ${domainClass.propertyName}: ${domainClass.propertyName}])
+	}
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: '${className}.label', default: '${className}'), ${propertyName}.id])
-                redirect ${propertyName}
-            }
-            '*'{ respond ${propertyName}, [status: OK] }
-        }
-    }
+	def salvar(${className} ${domainClass.propertyName}) {
 
-    @Transactional
-    def delete(${className} ${propertyName}) {
+		def retorno
 
-        if (${propertyName} == null) {
-            notFound()
-            return
-        }
+		if (params.${domainClass.propertyName}.id) {
 
-        ${propertyName}.delete flush:true
+			${className} old = ${className}.get(params.${domainClass.propertyName}.id);
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: '${className}.label', default: '${className}'), ${propertyName}.id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
-    }
+			if (old.version.toLong() > params.${domainClass.propertyName}.version.toLong()) {
 
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: '${domainClass.propertyName}.label', default: '${className}'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
-    }
+				retorno = UtilsMensagem.getMensagem("A ${domainClass.propertyName} já foi alterado por outro usuário!\nFavor canecelar esta operação e tentar novamente!", NotifyType.ERROR)
+
+				render retorno as JSON
+
+				return
+			}
+		}
+
+		if (${domainClass.propertyName}.hasErrors()) {
+
+			retorno = UtilsMensagem.getMensagem("Não foi possível salvar!", NotifyType.ERROR, ${domainClass.propertyName}.errors)
+		} else {
+
+			${domainClass.propertyName}.save(flush:true)
+
+			retorno = UtilsMensagem.getMensagem("Salvo com sucesso!", NotifyType.SUCCESS)
+		}
+
+		render retorno as JSON
+	}
+
+	@Secured("ROLE_DELETE_${className}")
+	def excluir() {
+
+		def retorno
+
+		${className} ${domainClass.propertyName} = ${className}.get(params.id)
+
+		try {
+
+			${domainClass.propertyName}.delete(flush:true)
+
+			retorno = UtilsMensagem.getMensagem("Excluido com sucesso!", NotifyType.SUCCESS)
+		} catch(Exception e) {
+
+			retorno = UtilsMensagem.getMensagem("Não foi possível excluir!", NotifyType.ERROR)
+		}
+
+		render retorno as JSON
+	}
+	
 }
